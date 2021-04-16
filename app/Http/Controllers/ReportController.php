ac64492af4568fa8e0eaf97905fea97660e68550
Jon Fackrell <jon.fackrell@gmail.com>
2021-04-16 09:57:53 -0600

    no message

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,12 +13,21 @@ class ReportController extends Controller
     public function quality()
     {
         $records = collect([]);
-        $collections = Collection::where('auto_calculate', 1)->pluck('acronym', 'name')->all();
+        $collections = Collection::all();
 
         Record::cataloged()->chunk(50, function($items) use (&$records, $collections){
+            $autoCollections = $collections->where('auto_calculate', 1)->pluck('acronym', 'name')->all();
+            $manualCollections = $collections->where('auto_calculate', 0)->pluck('acronym', 'name')->all();
             foreach($items as $item){
-                if(in_array($item->mCollection, array_keys($collections))){
-                    if(! Str::of($item->mFolderNumber)->trim()->startsWith($collections[$item->mCollection])){
+                if(in_array($item->mCollection, array_keys($autoCollections))){
+                    if(! Str::of($item->mFolderNumber)->trim()->startsWith($autoCollections[$item->mCollection])){
+                        $records->push($item);
+                    }
+                }else if(in_array($item->mCollection, array_keys($manualCollections))){
+                    if(! Str::of($item->mFolderNumber)->trim()->startsWith(Str::of($manualCollections[$item->mCollection])->explode('[')->first())){
+                        $records->push($item);
+                    }
+                    if(! Str::of($manualCollections[$item->mCollection])->matchAll('/[A-Z]{3}_/')->count() < 1){
                         $records->push($item);
                     }
                 }
